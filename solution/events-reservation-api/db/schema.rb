377e62cc390945ace5a87aa148de9a8e6cb35b82
Dan HE <dan_emilie@yahoo.com>
2019-07-14 16:26:39 +0200

    [API]user migration

--- solution/events-reservation-api/db/schema.rb
+++ solution/events-reservation-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_10_173214) do
+ActiveRecord::Schema.define(version: 2019_07_14_141152) do
 
   create_table "events", force: :cascade do |t|
     t.string "type"
@@ -23,6 +23,17 @@ ActiveRecord::Schema.define(version: 2019_07_10_173214) do
     t.integer "max_participants", default: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email", null: false
+    t.string "password_digest"
+    t.string "role", default: "user"
+    t.string "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
