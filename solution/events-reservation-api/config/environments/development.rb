0ffb79a4588e904644df49b74cf225f5d095765a
Dan HE <dan_emilie@yahoo.com>
2019-07-15 22:15:35 +0200

    use mailtrap

--- solution/events-reservation-api/config/environments/development.rb
+++ solution/events-reservation-api/config/environments/development.rb
@@ -30,12 +30,15 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
-  # Care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
-
-  config.action_mailer.delivery_method = :letter_opener
-
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => 'b0c7ae1bbaa055',
+    :password => '668a1ce4c33563',
+    :address => 'smtp.mailtrap.io',
+    :domain => 'smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
